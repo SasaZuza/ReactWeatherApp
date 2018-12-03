bfd532daa3928521bbb6abd8b62446879da10365
‘SasaZuza’ <sasazuzazule@gmail.com>
2018-12-03 20:51:31 +0100

    Updates

---
+++ precache-manifest.377d79c34d7a5638787c6dc5a69b9b4c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/ReactWeatherApp/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "cdb1065d1eb8afd63d04",
+    "url": "/ReactWeatherApp/static/css/main.98feb77f.chunk.css"
+  },
+  {
+    "revision": "ce6dede9b2f791cb458df9d0c6a88286",
+    "url": "/ReactWeatherApp/static/media/bg3.ce6dede9.jpg"
+  },
+  {
+    "revision": "bfede335d6e219397a5f",
+    "url": "/ReactWeatherApp/static/js/1.bfede335.chunk.js"
+  },
+  {
+    "revision": "8ff246e946d46ed7eb05",
+    "url": "/ReactWeatherApp/static/js/runtime~main.8ff246e9.js"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/ReactWeatherApp/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "cdb1065d1eb8afd63d04",
+    "url": "/ReactWeatherApp/static/js/main.cdb1065d.chunk.js"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/ReactWeatherApp/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/ReactWeatherApp/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/ReactWeatherApp/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "bfede335d6e219397a5f",
+    "url": "/ReactWeatherApp/static/css/1.d7722083.chunk.css"
+  },
+  {
+    "revision": "e4b91a07b84a182f1ae59f8f3c5e8f3c",
+    "url": "/ReactWeatherApp/index.html"
+  }
+];
