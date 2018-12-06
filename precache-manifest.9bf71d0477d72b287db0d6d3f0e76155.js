dfc78575c4ef2a673503bf9da7a1d05460063ead
‘SasaZuza’ <sasazuzazule@gmail.com>
2018-12-06 20:10:57 +0100

    Updates

--- precache-manifest.9bf71d0477d72b287db0d6d3f0e76155.js
+++ precache-manifest.9bf71d0477d72b287db0d6d3f0e76155.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "448c34a56d699c29117adc64c43affeb",
-    "url": "/ReactWeatherApp/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/ReactWeatherApp/static/media/glyphicons-halflings-regular.f4769f9b.eot"
   },
   {
-    "revision": "cdb1065d1eb8afd63d04",
-    "url": "/ReactWeatherApp/static/css/main.98feb77f.chunk.css"
+    "revision": "ea2d1ee9a7a728354986",
+    "url": "/ReactWeatherApp/static/css/main.962d2b3d.chunk.css"
   },
   {
     "revision": "ce6dede9b2f791cb458df9d0c6a88286",
@@ -20,20 +20,20 @@ self.__precacheManifest = [
     "url": "/ReactWeatherApp/static/js/runtime~main.8ff246e9.js"
   },
   {
-    "revision": "f4769f9bdb7466be65088239c12046d1",
-    "url": "/ReactWeatherApp/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/ReactWeatherApp/static/media/glyphicons-halflings-regular.448c34a5.woff2"
   },
   {
-    "revision": "cdb1065d1eb8afd63d04",
-    "url": "/ReactWeatherApp/static/js/main.cdb1065d.chunk.js"
-  },
-  {
-    "revision": "fa2772327f55d8198301fdb8bcfc8158",
-    "url": "/ReactWeatherApp/static/media/glyphicons-halflings-regular.fa277232.woff"
+    "revision": "ea2d1ee9a7a728354986",
+    "url": "/ReactWeatherApp/static/js/main.ea2d1ee9.chunk.js"
   },
   {
     "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
     "url": "/ReactWeatherApp/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/ReactWeatherApp/static/media/glyphicons-halflings-regular.fa277232.woff"
   },
   {
     "revision": "89889688147bd7575d6327160d64e760",
@@ -44,7 +44,7 @@ self.__precacheManifest = [
     "url": "/ReactWeatherApp/static/css/1.d7722083.chunk.css"
   },
   {
-    "revision": "e4b91a07b84a182f1ae59f8f3c5e8f3c",
+    "revision": "7d65b2090b8322bac2feda982b3e4e35",
     "url": "/ReactWeatherApp/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ReactWeatherApp/precache-manifest.377d79c34d7a5638787c6dc5a69b9b4c.js"
+  "/ReactWeatherApp/precache-manifest.9bf71d0477d72b287db0d6d3f0e76155.js"
 );
 
 workbox.clientsClaim();
